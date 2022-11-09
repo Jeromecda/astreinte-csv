50cd445fd0cd2ad7037f3720faef58cb3a6b5875
VASSEUR Jerome <jerome.vasseur@uhc-sante.fr>
2022-11-09 13:08:37 +0100

    init

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/check_form', name: 'check_form')]
+    public function checkForm(Request $request): Response
+    {
+        $requete = $request->attributes->all();
+        dd($requete);
+        return $this->render('home/check_form.html.twig', [
+            'requete' => $requete,
+        ]);
+    }
+    // #[Route('/home', name: 'app_home')]
+    // public function index(): JsonResponse
+    // {
+    //     return $this->json([
+    //         'message' => 'Test',
+            
+    //         'path' => 'src/Controller/HomeController.php',
+    //     ]);
+    // }
+}
